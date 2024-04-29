f756c5643bfd62673372b3b87df7c4b6443e9cb3
Windows-AI <161544101+Windows-AI@users.noreply.github.com>
2024-04-28 17:35:20 -0700

    Added Logging

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ from PIL import Image
 import io
 import base64
 from celery import Celery
-
+log_file = open('iris.log', 'w')
+def writeLog(string):
+    print(string)
+    log_file.write(string)
+writeLog("Loading YOLO") 
 # Load YOLO
 try:
     net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
@@ -19,13 +23,14 @@ except Exception as e:
 
 layer_names = net.getLayerNames() if net else []
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()] if net else []
-
+writeLog("Calling FastAPI") 
 app = FastAPI()
-
+writeLog("Loading Celery") 
 celery_app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 @celery_app.task
 def process_image(data):
+    writeLog("Processing Image") 
     image = Image.open(io.BytesIO(base64.b64decode(data)))
     image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
@@ -36,6 +41,7 @@ def process_image(data):
 
 @app.post('/image')
 async def upload_image(file: UploadFile = File(...)):
+    writeLog("Posting Image") 
     data = await file.read()
     result = process_image.delay(data)
     return {"task_id": str(result.id)}
